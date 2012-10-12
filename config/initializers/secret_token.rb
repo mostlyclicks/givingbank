835bbcb630879f1af80bdc6a89310cc82cbc4fd8
mostlyclicks <carlos@mostlyclicks.com>
2012-10-12 09:02:39 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Givingbank::Application.config.secret_token = '634140253d5705a9de10de05f2e42f1e178694385704a104e570b2f45cac81492034d9618ecf70ed263cd8512b8a8eeac314cb303e64a6ac717fccaf58ffef26'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Givingbank::Application.config.session_store :cookie_store, key: '_givingbank_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Givingbank::Application.config.session_store :active_record_store
